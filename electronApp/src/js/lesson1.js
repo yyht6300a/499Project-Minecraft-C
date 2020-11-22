be9c553644f380e47d7271a4ecde54c2ce30c90a
caincdiy <caoyueccc@gmail.com>
2020-11-22 06:05:12 -0800

    Gold digger lesson update

--- electronApp/src/js/lesson1.js
+++ electronApp/src/js/lesson1.js
@@ -12,6 +12,8 @@ var output = '';
 // Start server
 var serverShell = pythonShell.PythonShell;
 
+
+
 let options = {
   mode: 'text',
   pythonOptions: ['-u'], // get print results in real-time
@@ -25,6 +27,7 @@ serverShell.run("src/connection.py", options, function(err, results) {
   console.log(results)
 });
 
+
 // Load in lesson
 var lessonText = 'error'
 fs.readFile('src/lessonTemplates/lesson1.py', (err, data) => { 
@@ -34,8 +37,31 @@ fs.readFile('src/lessonTemplates/lesson1.py', (err, data) => {
   lessonText = data.toString();
 }) 
 // Run lesson code
+
+
+var lessonText_2='error'
+fs.readFile('src/lessonTemplates/Gold_Digger_1.py', (err, data) => { 
+  if (err) 
+    throw err; 
+  
+  lessonText_2 = data.toString();
+}) 
+
+var lessonText_3='error'
+fs.readFile('src/lessonTemplates/Gold_Digger_2.py', (err, data) => { 
+  if (err) 
+    throw err; 
+  
+  lessonText_3 = data.toString();
+}) 
+
+
 var pyShell = pythonShell.PythonShell;
+var pyShell_2 = pythonShell.PythonShell;
+var pyShell_3 = pythonShell.PythonShell;
+
 var outputText = 'error'
+
 
 pyShell.run("src/lessonTemplates/lesson1.py", options, function (err, results) {
   if (err) 
@@ -45,6 +71,28 @@ pyShell.run("src/lessonTemplates/lesson1.py", options, function (err, results) {
   outputText = results.join('\n');
   console.log(outputText);
 });
+
+
+var outputText_2='error'
+pyShell_2.run("src/lessonTemplates/Gold_Digger_1.py", options, function (err, results) {
+  if (err) 
+    throw err;
+    
+  // results is an array consisting of messages collected during execution
+  outputText_2 = results.join('\n');
+  console.log(outputText_2);
+});
+
+var outputText_3='error'
+pyShell_3.run("src/lessonTemplates/Gold_Digger_2.py", options, function (err, results) {
+  if (err) 
+    throw err;
+    
+  // results is an array consisting of messages collected during execution
+  outputText_3 = results.join('\n');
+  console.log(outputText_3);
+});
+
 
 
 // Setup for code editor
@@ -63,6 +111,7 @@ amdRequire.config({
 // workaround monaco-css not understanding the environment
 self.module = undefined;
 
+
 // Code editor
 amdRequire(['vs/editor/editor.main'], function () {
   // Coding environment
@@ -74,8 +123,10 @@ amdRequire(['vs/editor/editor.main'], function () {
     minimap: {
       enabled: false
     }
+
+    
   });
-  
+
   // Code output (may not be required)
   output = monaco.editor.create(document.getElementById('output'), {
     value: outputText,
@@ -101,4 +152,97 @@ amdRequire(['vs/editor/editor.main'], function () {
       output.setValue(outputText);
     });
   });
+
+
+
+
+
+
+  codeEnv_2 = monaco.editor.create(document.getElementById('coding-environment_2'), {
+    value: lessonText_2,
+    language: "python",
+    theme: "vs-dark",
+    automaticLayout: true,
+    minimap: {
+      enabled: false
+    }
+
+    
+  });
+  
+  output_2 = monaco.editor.create(document.getElementById('output_2'), {
+    value: outputText_2,
+    language: "python",
+    theme: "vs-dark",
+    automaticLayout: true,
+    minimap: {
+      enabled: false
+    }
+  });
+
+
+  document.getElementById("run_2").addEventListener('click', function(e) {
+    e.preventDefault();
+
+    pyShell_2.runString(codeEnv_2.getValue(), options, function (err, results) {
+      if (err) 
+        throw err;
+        
+      // results is an array consisting of messages collected during execution
+      outputText_2 = results.join('\n');
+      console.log(outputText_2);
+      output_2.setValue(outputText_2);
+    });
+  });
+
+
+
+
+  codeEnv_3 = monaco.editor.create(document.getElementById('coding-environment_3'), {
+    value: lessonText_3,
+    language: "python",
+    theme: "vs-dark",
+    automaticLayout: true,
+    minimap: {
+      enabled: false
+    }
+
+    
+  });
+  
+  output_3 = monaco.editor.create(document.getElementById('output_3'), {
+    value: outputText_3,
+    language: "python",
+    theme: "vs-dark",
+    automaticLayout: true,
+    minimap: {
+      enabled: false
+    }
+  });
+
+
+  document.getElementById("run_3").addEventListener('click', function(e) {
+    e.preventDefault();
+
+    pyShell_3.runString(codeEnv_3.getValue(), options, function (err, results) {
+      if (err) 
+        throw err;
+        
+      // results is an array consisting of messages collected during execution
+      outputText_3 = results.join('\n');
+      console.log(outputText_3);
+      output_3.setValue(outputText_3);
+    });
+  });
+
+
+
 });
+
+
+
+
+
+
+
+
---
+++ electronApp/src/lessonTemplates/Gold_Digger_2.py
@@ -0,0 +1,5 @@
+def printyourname(name): #this function will print your name twice 
+    for i in range(2):
+        print("Hi "+name)
+printyourname("Bob")
+print("end")
