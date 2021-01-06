6220342648b4ddffb30de041742745629f9a6fa2
Nicholas McGee <nicholas.r.mcgee@gmail.com>
2021-01-06 16:49:43 -0700

    Added loading in from JSON to lesson

--- webserver/js/main.js
+++ webserver/js/main.js
@@ -1,30 +1,9 @@
-// Open lessons from main page 
-document.getElementById("lesson1").addEventListener('click', function() {
-    showLessonPage();
-});
-
-document.getElementById("lesson2").addEventListener('click', function() {
-    showLessonPage();
-});
-
-document.getElementById("lesson3").addEventListener('click', function() {
-    showLessonPage();
-});
-
-
 // Code for editor
-// Go back to homepage button
-document.getElementById("homepage").addEventListener('click', function() {
-    document.getElementById("lesson-page").style.visibility="hidden";
-    document.getElementById("lesson-page").style.opacity="0";
-    document.getElementById("lesson-page").style.top="40%";
-    playBtnClick()
-});
-
 // Start the monaco-editor
+var editor = 0;
 require.config({ paths: { vs: '../node_modules/monaco-editor/min/vs' } });
 require(['vs/editor/editor.main'], function () {
-    var editor = monaco.editor.create(document.getElementById('editor'), {
+    editor = monaco.editor.create(document.getElementById('editor'), {
         value: ['def hello():', '\tprint("Hello world!");'].join('\n'),
         language: "python",
         theme: "vs-dark",
@@ -35,7 +14,48 @@ require(['vs/editor/editor.main'], function () {
     });
 });
 
+// Go back to homepage button
+document.getElementById("homepage").addEventListener('click', function() {
+    document.getElementById("lesson-page").style.visibility="hidden";
+    document.getElementById("lesson-page").style.opacity="0";
+    document.getElementById("lesson-page").style.top="40%";
+    playBtnClick()
+});
+
+
+// Main page
+// Open lessons from main page
+var lesson1Part = 0;
+document.getElementById("lesson1").addEventListener('click', function() {
+    setLessonData(1, lesson1Part);
+    showLessonPage();
+});
+
+var lesson2Part = 0;
+document.getElementById("lesson2").addEventListener('click', function() {
+    setLessonData(2, lesson2Part);
+    showLessonPage();
+});
+
+var lesson3Part = 0;
+document.getElementById("lesson3").addEventListener('click', function() {
+    setLessonData(3, lesson3Part);
+    showLessonPage();
+});
+
+
 // Helper functions
+async function getJSONData(url) {
+    const response = await fetch(url);
+    return response.json();
+}
+
+async function setLessonData(lesson, part) {
+    var lessonData = await getJSONData("lessons/lesson" + lesson + ".json");
+    document.getElementById("instructions").textContent = lessonData[part].instructions;
+    editor.setValue(lessonData[part].starterCode);
+}
+
 function showLessonPage() {
     document.getElementById("lesson-page").style.visibility="visible";
     document.getElementById("lesson-page").style.opacity="1";
